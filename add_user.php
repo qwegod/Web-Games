f901828ccb3b846e9406cba8fd510816134fbf0d
qwegod <128311741+qwegod@users.noreply.github.com>
2023-03-25 15:29:57 +0200

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -1,13 +1,23 @@
 <?php
-require_once 'register.php';
+require_once 'config.php';
 
-// Добавление информации о пользователе в базу данных
-$sql = "INSERT INTO users (username, password) VALUES ('user1', 'pass1')";
+// Проверяем, что данные формы регистрации отправлены методом POST
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  // Получаем данные из формы регистрации
+  $username = $_POST['username'];
+  $password = $_POST['password'];
 
-if (mysqli_query($conn, $sql)) {
-  echo "Новая запись успешно добавлена";
+  // Добавляем информацию о пользователе в базу данных
+  $sql = "INSERT INTO users (username, password) VALUES ('$username', '$password')";
+
+  if (mysqli_query($conn, $sql)) {
+    echo "Новая запись успешно добавлена";
+  } else {
+    echo "Ошибка: " . mysqli_error($conn);
+  }
+
+  mysqli_close($conn);
 } else {
-  echo "Ошибка: " . mysqli_error($conn);
+  // Если данные формы не отправлены методом POST, то выводим сообщение об ошибке
+  echo "Ошибка: данные формы не были отправлены методом POST";
 }
-
-mysqli_close($conn);
