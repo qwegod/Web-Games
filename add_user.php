da5e4819232b9e49cc37fc13f211eb93d271c6dc
qwegod <128311741+qwegod@users.noreply.github.com>
2023-03-25 15:25:48 +0200

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -1,1 +1,13 @@
-{\rtf1}
+<?php
+require_once 'register.php';
+
+// Добавление информации о пользователе в базу данных
+$sql = "INSERT INTO users (username, password) VALUES ('user1', 'pass1')";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Новая запись успешно добавлена";
+} else {
+  echo "Ошибка: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
